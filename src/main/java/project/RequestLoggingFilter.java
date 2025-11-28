7e3db0c21ec6bfd9e40cca19051209c4026e9570
Your Name <you@example.com>
2025-11-28 02:49:10 +0200

    branch lab 4 springboot

--- src/main/java/project/RequestLoggingFilter.java
+++ src/main/java/project/RequestLoggingFilter.java
@@ -12,7 +12,6 @@ public class RequestLoggingFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
-
         HttpServletRequest req = (HttpServletRequest) request;
         System.out.println("[LOG BEFORE] " + req.getMethod() + " " + req.getRequestURI() + " at " + LocalDateTime.now());
 
